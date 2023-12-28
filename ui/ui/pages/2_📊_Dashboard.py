f95b03568c439ed4a15a5c96b45a30ce954d9ed5
Tatiana <tatiana.ekeinhor@gmail.com>
2023-12-28 18:31:54 +0100

    feat: create first proto of UI using streamlit

---
+++ ui/ui/pages/2_📊_Dashboard.py
@@ -0,0 +1,33 @@
+"""Admin page for monitoring"""
+from pathlib import Path
+import streamlit as st
+import streamlit_authenticator as stauth
+
+print(Path(__file__).resolve())
+current_file = Path(__file__)
+config_file = current_file.parents[1]/"config/users.yaml"
+
+st.write("# Admin Dashboard! 👋")
+import yaml
+from yaml.loader import SafeLoader
+with open(str(config_file)) as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
---
+++ ui/ui/pages/3_👩🏾‍💻_About.py
@@ -0,0 +1,7 @@
+"""Admin page for monitoring"""
+
+
+import streamlit as st
+
+
+st.write("# Welcome to About! 👋")
