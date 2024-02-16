0186b17d5c30fbba86dad69fda7bb901d9353a4e
Tatiana <tatiana.ekeinhor@gmail.com>
2024-02-16 17:54:32 +0100

    fix: allow ci to create ECR repo if it doesn't exist

---
+++ .github/workflows/ci.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+create_ecr_repo () {
+    REPO_NAME=$1
+    echo "repository name: ${REPO_NAME}"
+    output=$(aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1) #redirect stdout in case of success and stderr otherwise, in output
+    status_code=$? # store the exit code of the previous cmd
+    if [ $status_code -ne 0 ]; then # cmd was not successfull
+        if echo ${output} | grep -q RepositoryNotFoundException; then
+            echo "echo repository doesn't exist, will create it"
+            aws ecr create-repository --repository-name ${REPO_NAME}
+            echo "created ${REPO_NAME}"
+        else
+        >&2 echo ${output} # redirect output to stderr if the error different from RepositoryNotFoundException
+        fi
+    else
+        echo "repository already exist"
+    fi
+}
