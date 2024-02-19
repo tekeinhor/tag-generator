c241018b26cfdb2d5b48c69096c086cf102a55cf
Tatiana <tatiana.ekeinhor@gmail.com>
2024-02-19 18:48:44 +0100

    test: remove inline comments

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+#redirect stdout in case of success and stderr otherwise, in output
+# store the exit code of the previous cmd
 create_ecr_repo () {
     REPO_NAME=$1
     echo "repository name: ${REPO_NAME}"
     echo "test aws version"
     aws --version
     echo "test an aws describe"
-    aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1
-    output=$(aws ecr describe-repositories --repository-names ${REPO_NAME}) #redirect stdout in case of success and stderr otherwise, in output
+    # aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1
     echo "another test"
-    output=$(aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1) #redirect stdout in case of success and stderr otherwise, in output
-    status_code=$? # store the exit code of the previous cmd
+    output=$(aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1)
+    status_code=$?
     echo "current output: ${output}"
     echo "status of cmd: ${status_code}"
     if [ $status_code -ne 0 ]; then # cmd was not successfull
@@ -19,7 +20,8 @@ create_ecr_repo () {
             #aws ecr create-repository --repository-name ${REPO_NAME}
             echo "created ${REPO_NAME}"
         else
-        >&2 echo ${output} # redirect output to stderr if the error different from RepositoryNotFoundException
+        # redirect output to stderr if the error different from RepositoryNotFoundException
+        >&2 echo ${output}
         fi
     else
         echo "repository already exist"
