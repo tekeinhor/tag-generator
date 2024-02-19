96d65ac4915411594e85ffa40cacc0e31559cd37
Tatiana <tatiana.ekeinhor@gmail.com>
2024-02-19 19:20:59 +0100

    fix(cicd): cancel fail-fast behavior of the github action in ecr management stage

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -3,9 +3,11 @@
 create_ecr_repo () {
     REPO_NAME=$1
     echo "repository name: ${REPO_NAME}"
-    output=$(aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1) #redirect stdout in case of success and stderr otherwise, in output
-    status_code=$? # store the exit code of the previous cmd
-    echo "current output: ${output}"
+    aws --version
+    #redirect stdout in case of success and stderr otherwise, in output
+    output=$(aws ecr describe-repositories --repository-names ${REPO_NAME} 2>&1)
+    # store the exit code of the previous cmd
+    status_code=$?
     echo "status of cmd: ${status_code}"
     if [ $status_code -ne 0 ]; then # cmd was not successfull
         if echo ${output} | grep -q RepositoryNotFoundException; then
@@ -13,7 +15,8 @@ create_ecr_repo () {
             aws ecr create-repository --repository-name ${REPO_NAME}
             echo "created ${REPO_NAME}"
         else
-        >&2 echo ${output} # redirect output to stderr if the error different from RepositoryNotFoundException
+        # redirect output to stderr if the error different from RepositoryNotFoundException
+        >&2 echo ${output}
         fi
     else
         echo "repository already exist"
